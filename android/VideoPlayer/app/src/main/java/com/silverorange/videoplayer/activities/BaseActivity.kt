9c121bd067b2c810041732161e3dac4de8ed6de5
Hamza <hamza.naveed@qavitechnologies.com>
2022-10-22 18:00:20 +0500

    ANDROID - Exoplayer setup
    setup exoplayer on the VideoInfoActivity

--- android/VideoPlayer/app/src/main/java/com/silverorange/videoplayer/activities/BaseActivity.kt
+++ android/VideoPlayer/app/src/main/java/com/silverorange/videoplayer/activities/BaseActivity.kt
@@ -2,9 +2,15 @@ package com.silverorange.videoplayer.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import dagger.android.AndroidInjection
+import javax.inject.Inject
 
 open class BaseActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var viewModelProvider: ViewModelProvider.Factory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AndroidInjection.inject(this)
--- android/VideoPlayer/app/src/main/java/com/silverorange/videoplayer/activities/video_detail/VideoInfoActivity.kt
+++ android/VideoPlayer/app/src/main/java/com/silverorange/videoplayer/activities/video_detail/VideoInfoActivity.kt
@@ -1,14 +1,105 @@
 package com.silverorange.videoplayer.activities.video_detail
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import com.silverorange.videoplayer.R
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.media3.common.util.Util
+import androidx.media3.exoplayer.ExoPlayer
 import com.silverorange.videoplayer.activities.BaseActivity
+import com.silverorange.videoplayer.databinding.ActivityVideoDetailBinding
 
 class VideoInfoActivity : BaseActivity() {
 
+    private lateinit var viewModel: VideoInfoViewModel
+
+    private var player: ExoPlayer? = null
+
+    private var playWhenReady = true
+    private var currentItem = 0
+    private var playbackPosition = 0L
+
+    private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityVideoDetailBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_video_detail)
+        setContentView(viewBinding.root)
+        viewModel = ViewModelProvider(this, viewModelProvider)[VideoInfoViewModel::class.java]
+        viewBinding.viewmodel = viewModel
+
+        setupViews()
+        initObserver()
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+
+    public override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
+    }
+
+    private fun setupViews() {
+        initializePlayer()
+    }
+
+    private fun initializePlayer() {
+        player = ExoPlayer.Builder(this)
+            .build()
+            .also { exoPlayer ->
+                viewBinding.videoView.player = exoPlayer
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.seekTo(currentItem, playbackPosition)
+                exoPlayer.prepare()
+            }
+    }
+
+    private fun initObserver() {
+        TODO("Not yet implemented")
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, viewBinding.videoView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    private fun releasePlayer() {
+        player?.let { exoPlayer ->
+            playbackPosition = exoPlayer.currentPosition
+            currentItem = exoPlayer.currentMediaItemIndex
+            playWhenReady = exoPlayer.playWhenReady
+            exoPlayer.release()
+        }
+        player = null
     }
 }
